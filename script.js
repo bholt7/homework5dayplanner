3c41fad3ff7b322d8e6c6eb85b87ecf634b8f7c2
Pain <holtbrenden@yahoo.com>
2022-02-19 17:36:09 -0700

    edits

--- script.js
+++ script.js
@@ -2,6 +2,53 @@ $(init);
 
 // display date and time 
 function init (){
+    
 $("#currentDay").text(moment().format("dddd, MMMM Do LTS"));
 
+color();
+setInterval(color, 60000);
+
 }
+
+
+function color () {
+
+    $(".time-block").each(function () {
+
+    var timeofDay = parseInt($(this).attr("id").replace("hour-"));
+
+    var immediate = parseInt(moment().format('H'));
+
+    if (timeofDay < immediate) {
+
+        $(this).attr("style", "background-color: teal");
+
+    } else if (timeofDay > immediate) {
+
+        $(this).attr("style", "background-color: goldenrod");
+
+    } else {
+        $(this).attr("style", "background-color: orange")
+    }
+
+});
+
+}
+
+
+$(".time-block").each(function () {
+    var id = $(this).attr("id");
+
+    var description = localStorage.getItem(id)
+});
+
+var saveBtn = document.querySelector(".savebtn");
+
+    $(saveBtn).on("click", function() {
+
+        var hour = $(this).parent().attr("id");
+        var description = $(this).siblings(".description").val();
+
+        localStorage.setItem(hour, description)
+
+    });
